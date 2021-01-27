4335b12ffe36740d81221ff6be83edd5c1fa30a5
scar217 <73574679+scar217@users.noreply.github.com>
2021-01-27 15:33:45 +0800

    Update Task1.java

--- week1/src/Task1.java
+++ week1/src/Task1.java
@@ -21,13 +21,13 @@ import java.util.*;
             if(BMI<18.5){
                 System.out.println("过轻");
             }
-            else if(BMI>=18.5||BMI<25){
+            else if(BMI>=18.5&&BMI<25){
                 System.out.println("正常");
             }
-            else if(BMI>=25||BMI<28){
+            else if(BMI>=25&&BMI<28){
                 System.out.println("过重");
             }
-            else if(BMI>=28||BMI<32){
+            else if(BMI>=28&&BMI<32){
                 System.out.println("肥胖");
             }
             else{
