df6fc80928a1aa83ca0ba6d9c0e9eaec6f381c7c
scar217 <1506523471@qq.com>
2021-01-30 21:25:47 +0800

    第一次提交

---
+++ week1/src/Task5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*定义一个 Circle 类，其中成员变量 radius 表示圆的半径，编写方法 getArea() 计
+算圆的面积、getPerimeter()计算圆的周长，并在主函数中构造一个 Circle 的对象进行测
+试。
+示例：
+输入：3
+输出：the area is 28.26
+the perimeter is 18.84
+ */
+public class Task5 {
+    public static void main(String[] args){
+        Scanner scanf = new Scanner(System.in);
+        double radius,N=3.14;
+        System.out.println("请输入圆的半径：");
+        radius = scanf.nextDouble();
+        Circle p1 = new Circle();
+        p1.getArea(radius,N);
+        p1.getPerimeter(radius,N);
+    }
+}
+class Circle{
+    public void getArea(double radius,double N){
+        double area;
+        area = N*radius*radius;
+        System.out.println("the area is "+area);
+    }
+    public void getPerimeter(double radius,double N){
+        double perimeter;
+        perimeter = 2*N*radius;
+        System.out.println("the perimeter is "+perimeter);
+    }
+}
